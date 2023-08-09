2bb2f21789ab439293a2fa8c2b9561697cf0166f
Husein Naimov <huseinnaimov@bk.ru>
2023-08-09 15:34:05 +0500

    project update

--- backend/index.py
+++ backend/index.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"message": "Hello World"}
+location / {
+proxy_pass http://localhost:8000;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+}
