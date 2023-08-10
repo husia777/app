375911da9af74839a52f479410fb9fd72241fbca
Husein Naimov <huseinnaimov@bk.ru>
2023-08-10 22:35:37 +0500

    project update

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App";
+
+const reactRoot = createRoot(document.getElementById("root")!);
+
+reactRoot.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
