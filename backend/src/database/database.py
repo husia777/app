bdec9d1914215e14287fc0d9d0bd404eca7e21ae
Husein Naimov <huseinnaimov@bk.ru>
2023-08-14 20:14:23 +0500

    workflow

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,15 +1,21 @@
 
+import datetime
+from sqlalchemy import JSON, TIMESTAMP, Integer, String
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, mapped_column
 from config import SQLALCHEMY_DATABASE_URL
 
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+
+# Базовая настройка
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 async def get_session() -> AsyncSession:
@@ -18,3 +24,20 @@ async def get_session() -> AsyncSession:
         yield session
     finally:
         session.close()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = mapped_column(Integer, primary_key=True, index=True)
+    email = mapped_column(String, unique=True, nullable=False)
+    username = mapped_column(String, nullable=False)
+    password = mapped_column(String, nullable=False)
+    registered = mapped_column(TIMESTAMP, default=datetime.utcnow)
+    role_id = mapped_column("Roles", back_populates="owner")
+
+
+class Roles(Base):
+    __tablename__ = 'roles'
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String, nullable=False)
+    permissions = mapped_column(JSON)
