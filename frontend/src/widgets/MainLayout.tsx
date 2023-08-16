1f47d7fee026684506306dcfe02376d9f3c184f6
Husein Naimov <huseinnaimov@bk.ru>
2023-08-16 13:28:22 +0500

    workflow

--- frontend/src/widgets/MainLayout.tsx
+++ frontend/src/widgets/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const MainLayout = () => {
 	return (
 		<div>
-			<h1 className="Main">Hi</h1>
+			<h1 className="Main">Hi 111</h1>
 			<img src="../../../art/84523ec08abfd5e288df30534621b6ca.jpg" />
 		</div>
 	);
