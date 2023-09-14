074ac8044a7e2ba33a5b49468c428f9bcbcdf869
Husein Naimov <huseinnaimov@bk.ru>
2023-09-14 23:25:18 +0500

    app

--- backend/src/resume/models.py
+++ backend/src/resume/models.py
@@ -1,4 +1,4 @@
-from database.database import Base
-from sqlalchemy import Mapped, mapped_column
-class Resume(Base):
-    name:Mapped[str] = mapped_column()
+# from database.database import Base
+# from sqlalchemy import Mapped, mapped_column
+# class Resume(Base):
+#     name:Mapped[str] = mapped_column()
