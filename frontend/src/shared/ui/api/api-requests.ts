e97d612fcc59f217ab60476c17100e434ef5e763
Husein Naimov <huseinnaimov@bk.ru>
2023-09-15 21:09:52 +0500

    app

--- frontend/src/shared/ui/api/api-requests.ts
+++ frontend/src/shared/ui/api/api-requests.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { config } from "dotenv";
 
-export const API_LOCALHOST_URL = `http://localhost:8080`;
+export const API_LOCALHOST_URL = `http://0.0.0.0:8080`;
 
 const $api = axios.create({
 	withCredentials: true,
