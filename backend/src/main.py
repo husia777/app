c7990cc6a26420b77d5b0ffe2bf31d22c43dd2f1
Husein Naimov <huseinnaimov@bk.ru>
2023-09-15 17:25:57 +0500

    app

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,8 +12,5 @@ origins = {
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
