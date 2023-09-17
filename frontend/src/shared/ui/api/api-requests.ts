203982e99ee9ac8d3f1b8645aadb4f6ed3fcf72e
Husein Naimov <huseinnaimov@bk.ru>
2023-09-17 16:23:07 +0500

    app

--- frontend/src/shared/ui/api/api-requests.ts
+++ frontend/src/shared/ui/api/api-requests.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { config } from "dotenv";
 
-export const API_LOCALHOST_URL = `https://huseinnaimov.com/api`;
+export const API_LOCALHOST_URL = `https://huseinnaimov.com`;
 
 const $api = axios.create({
 	withCredentials: true,
