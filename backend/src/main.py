18c6f5f29e1ef179e032480834b8d9cf522bdb98
Husein Naimov <huseinnaimov@bk.ru>
2023-09-18 11:18:53 +0500

    app

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,7 +9,7 @@ origins = ["http://huseinnaimov.com"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
