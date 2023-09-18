059d70d6648026ffff0210ef71a3d52bacb0daf1
Husein Naimov <huseinnaimov@bk.ru>
2023-09-18 11:37:36 +0500

    app

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,12 +7,12 @@ app = FastAPI()
 app.include_router(router=auth_router)
 origins = ["http://huseinnaimov.com"] 
 
-app.add_middleware(
-    CORSMiddleware,
-    # allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+# app.add_middleware(
+#     CORSMiddleware,
+#     # allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
     
-)
+# )
 
