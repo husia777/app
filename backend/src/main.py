1c38e846a660ff46f12b2e005834880bd4f64dc6
Husein Naimov <huseinnaimov@bk.ru>
2023-09-18 16:15:13 +0500

    app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth.router import router as auth_router
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 
 app.include_router(router=auth_router)
-origins = ["http://huseinnaimov.com"]
+origins = {
+    "http://localhost",
+    "http://localhost:3000",
+    "http://huseinnaimov.com",
+    "https://huseinnaimov.com"
+}
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://huseinnaimov.com"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-    allow_headers=["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
