175eb709fbffbed34256310d71aa3fb9f053a5f3
Husein Naimov <huseinnaimov@bk.ru>
2023-09-21 22:05:57 +0500

    app

--- frontend/src/shared/ui/api/api-requests.ts
+++ frontend/src/shared/ui/api/api-requests.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 import { config } from "dotenv";
-
+import { useAppDispatch, useAppSelector } from "../../../app/Store/redux-hook";
+import {
+	selectAccessToken,
+	selectRefreshToken,
+} from "../../../entities/session/model/auth-selectors";
+import { refreshThunk } from "features/auth/auth_refresh/models/refresh-thunk";
 export const API_LOCALHOST_URL = `http://huseinnaimov.com:8080`;
-// export const API_LOCALHOST_URL = `http://huseinnaimov.com/api/`;
 
 const $api = axios.create({
 	withCredentials: true,
 	baseURL: API_LOCALHOST_URL,
 	headers: {
 		"Access-Control-Allow-Origin": "http://huseinnaimov.com",
-		// "Origin": "http://huseinnaimov.com",
 		"Access-Control-Allow-Credentials": true,
 	},
 });
@@ -25,21 +28,21 @@ $api.interceptors.request.use((config) => {
 
 export { $api };
 
-// $api.interceptors.response.use(async (response) => {
-// 	const { status, config } = response;
-// 	const dispatch = useAppDispatch();
+$api.interceptors.response.use(async (response) => {
+	const { status, config } = response;
+	const dispatch = useAppDispatch();
 
-// 	if (status === 401) {
-// 		const refreshToken = useAppSelector(selectRefreshToken);
+	if (status === 401) {
+		const refreshToken = useAppSelector(selectRefreshToken);
 
-// 		if (refreshToken) {
-// 			dispatch(refreshThunk(refreshToken));
-// 		}
-// 	}
-// 	const token = useAppSelector(selectAccessToken);
-// 	if (token) {
-// 		config.headers.Authorization = `Bearer ${token}`;
-// 	}
+		if (refreshToken) {
+			dispatch(refreshThunk(refreshToken));
+		}
+	}
+	const token = useAppSelector(selectAccessToken);
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
 
-// 	return response;
-// });
+	return response;
+});
