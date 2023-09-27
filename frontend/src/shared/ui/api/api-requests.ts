8f8b23a0969bd4b4f13292d5fc11438462b7c69c
Husein Naimov <huseinnaimov@bk.ru>
2023-09-27 23:11:42 +0500

    app

--- frontend/src/shared/ui/api/api-requests.ts
+++ frontend/src/shared/ui/api/api-requests.ts
@@ -20,7 +20,7 @@ const $api = axios.create({
 $api.interceptors.request.use((config) => {
 	const token = localStorage.getItem("accessToken");
 	if (token) {
-		config.headers.Authorization = `Bearer ${token}`;
+		config.headers.Authorization = ` Bearer ${token}`;
 	}
 
 	return config;
