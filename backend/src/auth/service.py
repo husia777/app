7801455f8ffb935b8cbd8b6c1ad226a41462343b
Husein Naimov <huseinnaimov@bk.ru>
2023-09-30 20:41:48 +0500

    app

--- backend/src/auth/service.py
+++ backend/src/auth/service.py
@@ -30,7 +30,7 @@ async def get_api_key_header(authorization: Annotated[str | None, Header(...)]) 
 
 async def get_current_user(token: str = Depends(get_api_key_header), session: AsyncSession = Depends(get_session)) -> schemas.User:
     token_data = AuthService.verify_token(token)
-    user_id = int(token_data['sub']['id'])
+    user_id = int(token_data['id'])
     user = await session.execute(select(models.User).where(models.User.id == user_id))
     user = user.scalar()
     return user
