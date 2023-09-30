5a6ab9244617b3cc677e686b297cf455693a5b68
Husein Naimov <huseinnaimov@bk.ru>
2023-09-30 21:13:32 +0500

    app

--- backend/src/auth/service.py
+++ backend/src/auth/service.py
@@ -39,6 +39,7 @@ async def get_current_user(token: str = Depends(get_api_key_header), session: As
     print()
     print(token_data)
     print(token_data.split(" "))
+    print(token_data.split(" ")[1])
     print(dict(token_data))
     print()
     print()
