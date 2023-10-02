bee5c7428f3ffdb1f37d546543d757f1b54f2f3f
Husein Naimov <huseinnaimov@bk.ru>
2023-10-02 17:05:41 +0500

    app

--- frontend/src/features/auth/account_confirmation/ui/account-confirmation-button/account-confirmation-button.tsx
+++ frontend/src/features/auth/account_confirmation/ui/account-confirmation-button/account-confirmation-button.tsx
@@ -5,7 +5,10 @@ import { useNavigate } from "react-router";
 import React from "react";
 import { accountConfirmationThunk } from "../../models/account-confirmation-thunk";
 import style from "./account-confirmation-form.module.scss";
-
+import {
+	CustomToastContainer,
+	successAlert,
+} from "../../../../../shared/ui/customAlert/custom-alert";
 export const ButtonSendConfirmCodeToEmail = () => {
 	const userData = getUserData();
 	const email = userData?.email;
@@ -14,15 +17,19 @@ export const ButtonSendConfirmCodeToEmail = () => {
 	const onClick = () => {
 		if (email) {
 			dispatch(accountConfirmationThunk(email));
+			successAlert(`На вашу почту ${email} был отправлен код подтверждения`)
 		}
 	};
 	return (
-		<Button
-			content="Получить код"
-			onClick={() => onClick()}
-			type="button"
-			disabled={false}
-			className={style.button}
-		/>
+		<>
+			<Button
+				content="Получить код"
+				onClick={() => onClick()}
+				type="button"
+				disabled={false}
+				className={style.button}
+			/>
+			<CustomToastContainer />
+		</>
 	);
 };
