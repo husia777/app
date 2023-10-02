d968c6646eaf5cc1497ff3708585ddf09b82ff77
Husein Naimov <huseinnaimov@bk.ru>
2023-10-02 13:47:25 +0500

    app

--- backend/src/auth/router.py
+++ backend/src/auth/router.py
@@ -52,7 +52,7 @@ async def logout():
 
 
 @router.post("/refresh/", status_code=status.HTTP_200_OK)
-async def get_new_access_token(token: Token, auth_service: AuthService = Depends()):
+async def get_new_access_token(token: Token.access_token, auth_service: AuthService = Depends()):
     return await auth_service.get_new_access_token(token.access_token)
 
 
